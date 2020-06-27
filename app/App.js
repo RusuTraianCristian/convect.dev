2ea38358ca8c913abbe164a6869925e75c22379a
Rusu Traian Cristian <rusutraiancristian@gmail.com>
2020-06-27 12:34:59 +0300

    first

---
+++ app/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import './App.css';
+
+const App = () => {
+    return (
+        <Fragment>
+            this is the APP component..
+        </Fragment>
+    );
+}
+
+export default App;
